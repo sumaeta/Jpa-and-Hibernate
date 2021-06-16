495893b9d0749eb8249f372f00a761383ce99b72
sumaeta <brenosumaeta33@gmail.com>
2021-06-16 16:01:47 -0300

    First commit

---
+++ src/main/java/com/exemplo/jpa_and_hibernate/JpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.jpa_and_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAndHibernateApplication.class, args);
+	}
+
+}
